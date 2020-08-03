a386fe9a3c12e4ec8c8d3e90aa839b315432839b
Tahmid Zahin <tzahin10@gmail.com>
2020-08-04 04:55:39 +0600

    version 4

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Syncfusion.UI.Xaml.Controls.Input;
-using Windows.UI;
+﻿using Windows.UI;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 using Windows.Devices.SerialCommunication;
@@ -11,6 +10,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media.Animation;
+using Microsoft.Toolkit.Uwp.UI.Animations.Behaviors;
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace hvac_version_2
@@ -22,6 +23,8 @@ namespace hvac_version_2
     {
         SolidColorBrush greenBrush = new SolidColorBrush(Colors.Green);
         SolidColorBrush redBrush = new SolidColorBrush(Colors.Red);
+        SolidColorBrush greyBrush = new SolidColorBrush(Colors.DarkSlateGray);
+
         //private bool updatingValues;
 
 
@@ -31,37 +34,78 @@ namespace hvac_version_2
         DataReader dataReaderObject = null;
         public string strFromPort;
         private string final_string;
+        
+        private int compressorAkey=0;
+        private int compressorBkey=0;
+        private int pump_entry = 0;
+        private int compAentry=0;
+        private int compBentry=0;
+        private int fan1entry = 0;
+        private int fan2entry = 0;
+        private double Blur_amount = 0;
 
         public MainPage()
         {
             this.InitializeComponent();
-
-                        
+            stop_button.IsEnabled = false;
+            set_temperature.IsEnabled = false;
+            set_switching_time.IsEnabled = false;
+            auto_mode.IsEnabled = false;
+            manual_mode.IsEnabled = false;
+            compressorA_button.IsEnabled = false;
+            compressorB_button.IsEnabled = false;
+            fanA_button.IsEnabled = false;
+            fanB_button.IsEnabled = false;
+            init.IsEnabled = false;
+            init_timing.IsEnabled = false;
+            pump_button.IsEnabled = false;
+            load_animation.Begin();
+            
         }
 
         private async void stop_button_Click(object sender, RoutedEventArgs e)
         {
             //do something
+            start_animation.Stop();
             if (serialPort == null) return;
             await sendToPort("end");
+            set_temperature.IsEnabled = false;
+            set_switching_time.IsEnabled = false;
+            manual_mode.IsEnabled = true;
+            auto_mode.IsEnabled = true;
+            stop_button.IsEnabled = false;
+            compressorA_button.IsEnabled = false;
+            compressorB_button.IsEnabled = false;
+            fanA_button.IsEnabled = false;
+            fanB_button.IsEnabled = false;
+            init.IsEnabled = false;
+            init_timing.IsEnabled = false;
+            pump_button.IsEnabled = false;
+            start_button.IsEnabled = true;
+            pump_entry = 0;
+            compAentry = 0;
+            compBentry = 0;
+            fan1entry = 0;
+            fan2entry = 0;
+            stop_animation.Begin();
+            start_button.Width = 162;
+            GC.Collect();
         }
 
-        private async void compressorA_button_Click(object sender, RoutedEventArgs e)
+        
+        
+        private async void set_temperature_changed(object sender, EventArgs e)
         {
-            if (serialPort == null) return;
-            await sendToPort("compressorAON");
-        }
-
-        private void set_temperature_changed(object sender, EventArgs e)
-        {
-            pump_status_light.Fill = redBrush;
+            //System.Diagnostics.Debug.WriteLine("entered the async void set_temp.............");
+            
+            
         }
 
         private async void set_switching_time_value_changed(object sender, EventArgs e)
-        {   
-            double switching_time_minute = (double)(set_switching_time.Value);
-            if (serialPort == null) return;
-            await sendToPort("switch_interval,"+ switching_time_minute);
+        {
+            //System.Diagnostics.Debug.WriteLine("entered the async void switching time.............");
+            
+            
 
         }
 
@@ -69,47 +113,130 @@ namespace hvac_version_2
         {
             if (serialPort == null) return;
             await sendToPort("manual");
+            auto_mode.IsEnabled = false;
+            manual_mode.IsEnabled = false;
+            stop_button.IsEnabled = true;
+            set_temperature.IsEnabled = true;
+            set_switching_time.IsEnabled = true;
+            pump_button.IsEnabled = true;
+            fanA_button.IsEnabled = true;
+            fanB_button.IsEnabled = true;
+            compressorA_button.IsEnabled = true;
+            compressorB_button.IsEnabled = true;
+            init.IsEnabled = true;
+            init_timing.IsEnabled = true;
+            
+        }
+
+        private async void auto_mode_Click(object sender, RoutedEventArgs e)
+        {
+            set_switching_time.IsEnabled = true;
+            set_temperature.IsEnabled = true;
+            init.IsEnabled = true;
+            stop_button.IsEnabled = true;
+            init_timing.IsEnabled = true;
+            if (serialPort == null) return;
+            await sendToPort("auto");
 
         }
 
-        private void auto_mode_Click(object sender, RoutedEventArgs e)
+        private async void start_button_Click(object sender, RoutedEventArgs e)
         {
-
+            stop_animation.Stop();
+            await sendToPort("ready");
+            auto_mode.IsEnabled = true;
+            manual_mode.IsEnabled = true;
+            start_button.IsEnabled = false;
+            start_animation.Begin();
+            System.Diagnostics.Debug.WriteLine("sending ready command");
+            
+            
         }
 
-        private void start_button_Click(object sender, RoutedEventArgs e)
+        private async void compressorA_button_Click(object sender, RoutedEventArgs e)
         {
-
+            if (compAentry == 0)
+            {
+                if (serialPort == null) return;
+                await sendToPort("compressorAON");   //sending key
+                compAentry = 1;
+            }
+            else
+            {
+                if (serialPort == null) return;
+                await sendToPort("compressorAoff");  //sending key
+                compAentry = 0;
+            }
         }
 
         private async void compressorB_button_Click(object sender, RoutedEventArgs e)
         {
-            if (serialPort == null) return;
-            await sendToPort("compressorBON");
+            if (compBentry == 0)
+            {
+                if (serialPort == null) return;
+                await sendToPort("compressorBON");   //sending key
+                compBentry = 1;
+            }
+            else
+            {
+                if (serialPort == null) return;
+                await sendToPort("compressorBoff");  //sending key
+                compBentry = 0;
+            }
         }
 
         private async void fanA_button_Click(object sender, RoutedEventArgs e)
         {
-            if (serialPort == null) return;
-            await sendToPort("fan1on");
+            if (fan1entry == 0)
+            {
+                if (serialPort == null) return;
+                await sendToPort("fan1on"); 
+                fan1entry = 1;
+            }
+            else
+            {
+                if (serialPort == null) return;
+                await sendToPort("fan1off");  
+                fan1entry = 0;
+            }
         }
 
         private async void fanB_button_Click(object sender, RoutedEventArgs e)
         {
-            if (serialPort == null) return;
-            await sendToPort("fan2on");
+            if (fan2entry == 0)
+            {
+                if (serialPort == null) return;
+                await sendToPort("fan2on");
+                fan2entry = 1;
+            }
+            else
+            {
+                if (serialPort == null) return;
+                await sendToPort("fan2off");
+                fan2entry = 0;
+            }
         }
 
         private async void pump_button_Click(object sender, RoutedEventArgs e)
         {
-            if (serialPort == null) return;
-            await sendToPort("pumpon");
+            if (pump_entry == 0)
+            {
+                if (serialPort == null) return;
+                await sendToPort("pumpon");
+                pump_entry = 1;
+            }
+            else
+            {
+                if (serialPort == null) return;
+                await sendToPort("pumpoff");
+                pump_entry = 0;
+            }
 
         }
 
         private async void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            string qFilter = SerialDevice.GetDeviceSelector("COM6");
+            string qFilter = SerialDevice.GetDeviceSelector("UART0");
             DeviceInformationCollection devices = await DeviceInformation.FindAllAsync(qFilter);
 
             if (devices.Any())
@@ -138,15 +265,15 @@ namespace hvac_version_2
             if (serialPort != null)
             {
                 serialPort.WriteTimeout = TimeSpan.FromMilliseconds(1000);
-                serialPort.ReadTimeout = TimeSpan.FromMilliseconds(1000);
+                serialPort.ReadTimeout = TimeSpan.FromMilliseconds(50);
                 serialPort.BaudRate = 115200;
                 serialPort.Parity = SerialParity.None;
                 serialPort.StopBits = SerialStopBitCount.One;
                 serialPort.DataBits = 8;
                 serialPort.Handshake = SerialHandshake.None;
                 //Console.WriteLine("Serial port configured successfully");
-                txtStatus.Text = "Serial port configured successfully";
-
+                //txtStatus.Text = "Serial port configured successfully";
+                
 
             }
         }
@@ -196,12 +323,14 @@ namespace hvac_version_2
             {
                 strFromPort = dataReaderObject.ReadString(bytesRead);
                 final_string = final_string + strFromPort;
-                if(final_string.Contains(Environment.NewLine))
+                //txtStatus2.Text = final_string;
+                if (final_string.Contains(Environment.NewLine))
                 {
                     string logicstring = final_string;
                     final_string = "";
 
-                    System.Diagnostics.Debug.WriteLine("writing logic string " + logicstring);
+                    //System.Diagnostics.Debug.WriteLine("writing logic string " + logicstring);
+                    
                     if (logicstring.StartsWith("A"))
                     {   
                         if(logicstring.Contains("-"))
@@ -257,8 +386,29 @@ namespace hvac_version_2
                     if (logicstring.StartsWith("pumpon"))
                     {
                         pump_status_light.Fill = greenBrush;
-                    }
+                        Storyboard_Pump_Led.RepeatBehavior = RepeatBehavior.Forever;
+                        Storyboard_Pump_Led.Begin();
 
+                    }
+                    if (logicstring.StartsWith("pumpoff"))
+                    {   
+                        pump_status_light.Fill = redBrush;
+                        Storyboard_Pump_Led.Stop();
+                    }
+                    if (logicstring.StartsWith("pumpfail"))
+                    {
+                        if (!FailPopup.IsOpen) 
+                        { 
+                            FailPopup.IsOpen = true; 
+                        }
+                    }
+                    if (logicstring.StartsWith("clear the popup"))
+                    {
+                        if (FailPopup.IsOpen) 
+                        {
+                            FailPopup.IsOpen = false;
+                        }
+                    }
                     if (logicstring.StartsWith("switch_interval"))
                     {
                         
@@ -269,7 +419,116 @@ namespace hvac_version_2
 
 
                     }
+                    if (logicstring.StartsWith("timer"))
+                    {
 
+
+                        logicstring = Regex.Match(logicstring, @"\d+").Value;
+                        int remaining_time = int.Parse(logicstring);
+                        remaining_time = remaining_time / 1000;
+                        timer_box.Text = remaining_time.ToString();
+
+
+                    }
+                    if (logicstring.StartsWith("Free RAM = "))
+                    {
+
+
+                        logicstring = Regex.Match(logicstring, @"\d+").Value;
+                        int remaining_memory = int.Parse(logicstring);
+                        free_memory.Text = remaining_memory.ToString();
+
+
+                    }
+                    if (logicstring.StartsWith("fan1on"))
+                    {
+                        fan1_status_light.Fill = greenBrush;
+                        storyboard_fan1_led.RepeatBehavior = RepeatBehavior.Forever;
+                        storyboard_fan1_led.Begin();
+                        
+                    }
+                    if (logicstring.StartsWith("fan1off"))
+                    {
+                        fan1_status_light.Fill = redBrush;
+                        storyboard_fan1_led.Stop();
+                    }
+                    if (logicstring.StartsWith("fan2on"))
+                    {
+                        fan2_status_light.Fill = greenBrush;
+                        storyboard_fan_2.RepeatBehavior = RepeatBehavior.Forever;
+                        storyboard_fan_2.Begin();
+                    }
+                    if (logicstring.StartsWith("fan2off"))
+                    {
+                        fan2_status_light.Fill = redBrush;
+                        storyboard_fan_2.Stop();
+                    }
+                    if (logicstring.StartsWith("compressorAON"))   //key
+                    {
+                        compressorA_status_light.Fill = greyBrush;
+                        compressorAkey = 1;
+                    }
+                    if (logicstring.StartsWith("compressorAoff"))  //key
+                    {
+                        compressorA_status_light.Fill = redBrush;
+                        compressorAkey = 0;
+                    }
+                    if (logicstring.StartsWith("compressorBON"))  //key
+                    {
+                        compressorB_status_light.Fill = greyBrush;
+                        compressorBkey = 1;
+                    }
+                    if (logicstring.StartsWith("compressorBoff")) //key
+                    {
+                        compressorB_status_light.Fill = redBrush;
+                        compressorBkey = 0;
+                    }
+                    if (logicstring.StartsWith("compressorA on")) // actually compressor on
+                    {
+                        compressorA_status_light.Fill = greenBrush;
+                        storyboard_compA_Led.RepeatBehavior = RepeatBehavior.Forever;
+                        storyboard_compA_Led.Begin();
+                    }
+                    if (logicstring.StartsWith("compressorB on")) // actually compressor on
+                    {
+                        compressorB_status_light.Fill = greenBrush;
+                        storyboard_compB_led.RepeatBehavior = RepeatBehavior.Forever;
+                        storyboard_compB_led.Begin();
+                    }
+
+                    if (logicstring.StartsWith("compressor off")) // actually compressor off
+                    {
+                        if (compressorBkey == 1)
+                        {
+                            compressorB_status_light.Fill = greyBrush;
+                            storyboard_compB_led.Stop();
+                        }
+                        if (compressorAkey == 1)
+                        {
+                            compressorA_status_light.Fill = greyBrush;
+                            storyboard_compA_Led.Stop();
+                        }
+                        if (compressorBkey == 0)
+                        {
+                            compressorB_status_light.Fill = redBrush;
+                            storyboard_compB_led.Stop();
+                        }
+                        if (compressorAkey == 0)
+                        {
+                            compressorA_status_light.Fill = redBrush;
+                            storyboard_compA_Led.Stop();
+                        }
+                    }
+                    if (logicstring.StartsWith("compressorA off")) // actually compressor off
+                    {
+                        compressorA_status_light.Fill = redBrush;
+                        storyboard_compA_Led.Stop();
+                    }
+                    if (logicstring.StartsWith("compressorB off")) // actually compressor off
+                    {
+                        compressorB_status_light.Fill = redBrush;
+                        storyboard_compB_led.Stop();
+                    }
                 }
             }
         }
@@ -342,10 +601,26 @@ namespace hvac_version_2
             serialPort = null;
         }
 
-        private void init_Click(object sender, RoutedEventArgs e)
+        private async void init_Click(object sender, RoutedEventArgs e)
         {
+            double sendtempvalue = (double)set_temperature.Value;
+            if (serialPort == null) return;
+            await sendToPort("settemp," + sendtempvalue);
+            //System.Diagnostics.Debug.WriteLine("EEPROM " + sendtempvalue);
+
             
+
         }
+
+        private async void init_timing_Click(object sender, RoutedEventArgs e)
+        {
+            double switching_time_minute = (double)set_switching_time.Value;
+            if (serialPort == null) return;
+            await sendToPort("switch_interval," + switching_time_minute);
+            //System.Diagnostics.Debug.WriteLine("EEPROM " + switching_time_minute);
+        }
+
+        
 
         /*
         private void Page_Unloaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("hvac_version_2")]
+[assembly: AssemblyTitle("hvac_version_4")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("hvac_version_2")]
+[assembly: AssemblyProduct("hvac_version_4")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
